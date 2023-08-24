38b4e92eb3ec42a9b5f5c3afb198edf3967614d7
Danila Kurosh <dan_kurosh@mail.ru>
2023-08-24 14:31:51 +0300

    First commit

---
+++ functions_telephone_directory.py
@@ -0,0 +1,66 @@
+import pandas as pd
+
+
+def add_note(base: pd.DataFrame):
+    # need to make separate data later, it for faster work
+    new_note = input('\nEnter a list element separated by ", ": ')
+    new_note = new_note.split(sep=', ')
+    print(f"\nYou added {new_note} to telephone directory")
+    base.loc[len(base.index)] = new_note
+
+
+def delete_note(base: pd.DataFrame):
+    # function for delete by index. You can delete few or one data
+    delete_index = (input('\nEnter index of data(if you want to delete few rows, '
+                          'write it with space): ')).split(sep=' ')
+    delete_index = [int(i) for i in delete_index]
+    base.drop(labels=delete_index, axis=0, inplace=True)
+    base.reset_index(drop=True, inplace=True)
+    print(f'\nYou deleted data by {delete_index} index.')
+
+
+def edit_note(base: pd.DataFrame):
+    edit_index = int(input('\nWrite index of data, that you want to change: '))
+    print(base.query(f'index == {edit_index}').to_string())
+    edit_agree = input('\nYou want to change this data? yes/no: ')
+    if edit_agree == 'yes':
+        pass
+    elif edit_agree == 'no':
+        pass
+    else:
+        print('\nError! Wrong answer.')
+
+
+def find_note(base: pd.DataFrame):
+    find_criteria = input('\nSurname - 1,\nName - 2,\nPatronymic - 3,\nOrganization name - 4,\n'
+                          'Work phone number - 5,\nPersonal phone number - 6\n'
+                          'Please, enter number, you want to find by: ')
+
+    if find_criteria.isdigit():
+        find_criteria = int(find_criteria)
+        find_word = input('\nEnter word or number you want find by: ')
+        find_set = {1: 'surname', 2: 'name', 3: 'patronymic',
+                    4: 'organisation_name', 5: 'work_phone_number', 6: 'personal_phone_number'}
+
+        find_df = pd.DataFrame
+        if find_criteria == 5 or find_criteria == 6:
+            find_df = base.query(f'{find_set[find_criteria]} == {find_word}')
+            if find_df.empty:
+                print('\nSorry, there is no data that you want to find :(')
+            else:
+                print(find_df.to_string())
+        elif 5 > find_criteria > 0:
+            find_df = base.query(f'{find_set[find_criteria]} == "{find_word}"')
+            if find_df.empty:
+                print('\nSorry, there is no data that you want to find :(')
+            else:
+                print(find_df.to_string())
+        else:
+            print('\nError! Wrong number.')
+    else:
+        print('Error! Wrong number!')
+
+
+def menu():
+    pass
+
---
+++ main.py
@@ -0,0 +1,49 @@
+# test program for phone book
+import pandas as pd
+
+from functions_telephone_directory import (add_note, delete_note,
+                                           edit_note, menu, find_note)
+
+# new columns and dataframe for the first time, then it's no need after first save
+'''
+col = ['surname', 'name', 'patronymic',
+       'organisation_name', 'work_phone_number', 'personal_phone_number']
+db = pd.DataFrame(columns=col)
+'''
+
+# reading from csv database
+db = pd.read_csv('test_db_csv.csv', index_col=0)
+
+# new data for test for the first time
+'''
+db.loc[len(db.index)] = ['Kurosh', 'Danila', 'Dmitriyevich',
+                         'Big Company', '84997770707', '88005553535']
+# print(db.to_string())
+'''
+
+print('\nHello, its telephone directory!')
+# main
+while True:
+    user_input = input('\nWhat you wanna do? Write it here '
+                       '(if you want to stop, write "stop"): ')
+    if user_input == 'print':
+        print(db.to_string())
+    elif user_input == 'add':
+        add_note(db)
+    elif user_input == 'delete':
+        delete_note(db)
+    elif user_input == 'edit':
+        edit_note(db)
+    elif user_input == 'find':
+        find_note(db)
+    elif user_input == 'stop':
+        decide = input('\nDo you want to save file? yes/no: ')
+        if decide == 'yes':
+            db.to_csv('test_db_csv.csv')
+            break
+        elif decide == 'no':
+            break
+        else:
+            print('\ni dont understand you')
+    else:
+        print('\ni dont understand you')
